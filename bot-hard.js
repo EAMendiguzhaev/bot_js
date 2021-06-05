5718aca591b0eaf3b4c4b4d6d056b222763d19dc
EAMendiguzhaev <mendiguzhaevea@gmail.com>
2021-06-05 13:41:32 +0300

    Доработка бота

--- bot-hard.js
+++ bot-hard.js
@@ -4,52 +4,54 @@ const isNumber = function (n) {
 };
 
 const getRandomNumber = function (num) {
-  num = Math.floor(Math.random() * 101);
-  console.log(num); // необязательно выводить
-  return num;
+  return (num = Math.floor(Math.random() * 101));
 };
 
-const numbeOfAttempts = 3;
+const numbeOfAttempts = function (attempts) {
+  return (attempts = 10); // количество попыток
+};
 
-const checkFunction = (hiddenNumber) => {
-  return function one(attempts) {
+const checkFunction = (hiddenNumber, attempts) => {
+  return function check() {
     if (attempts > 0) {
       const question = prompt("Угадай число от 1 до 100");
       if (question === null) {
-        return alert("Игра окончена");
+        alert(`Игра окончена. Загаданное число было ${hiddenNumber}.`);
       } else if (question > hiddenNumber) {
         attempts--;
         alert(
           `Загаданное число меньше, осталось попыток ${attempts}. Попробуйте снова!`
         );
-        checkFunction(hiddenNumber);
+        check();
       } else if (question < hiddenNumber) {
         attempts--;
         alert(
           `Загаданное число больше, осталось попыток ${attempts}. Попробуйте снова!`
         );
-        checkFunction(hiddenNumber);
+        check();
       } else if (!isNumber(question)) {
         alert("Введи число!");
-        checkFunction(hiddenNumber);
+        check();
       } else if (+question === hiddenNumber) {
-        const questionAfterWinning = сonfirm(
+        const questionAfterWinning = confirm(
           "Поздравляю, Вы угадали!!! Хотели бы сыграть еще?"
         );
         if (questionAfterWinning === true) {
-          startBot(numbeOfAttempts);
+          hiddenNumber = Math.floor(Math.random() * 101);
+          attempts = 10;
+          startBot();
         } else {
           alert("До скорого!");
         }
       }
-
-      one(attempts);
     } else if (attempts <= 0) {
       const questionAfterLosing = confirm(
         "Попытки закончились, хотите сыграть еще?"
       );
       if (questionAfterLosing === true) {
-        startBot(numbeOfAttempts);
+        hiddenNumber = Math.floor(Math.random() * 101);
+        attempts = 10;
+        startBot();
       } else {
         alert("До скорого!");
       }
@@ -57,6 +59,6 @@ const checkFunction = (hiddenNumber) => {
   };
 };
 
-const startBot = checkFunction(getRandomNumber());
-startBot(numbeOfAttempts);
+const startBot = checkFunction(getRandomNumber(), numbeOfAttempts());
+startBot();
 console.dir(startBot);
